24ee0809d6e9464eda6a2f9b5a902dc63d987469
Azamat <azamat201760@yandex.ru>
2021-08-19 08:00:46 +0500

    d

--- Tcp Send/tcpsend/tcpsend.ino
+++ Tcp Send/tcpsend/tcpsend.ino
@@ -8,11 +8,13 @@
 #define STAPSK  "R00000000"
 #endif
 
+WiFiServer server(9090);
+
 const char* ssid     = STASSID;
 const char* password = STAPSK;
 
-const char* host = "djxmmx.net";
-const uint16_t port = 8080;
+const char* host = "";
+
 
 const uint8_t pin_ENA = D6;
 const uint8_t pin_DIR = D7;
@@ -63,107 +65,85 @@ void setup() {
 }
 
 void loop() {
-  static bool wait = false;
+  WiFiClient client = server.available();
 
-  Serial.print("connecting to ");
-  Serial.print(host);
-  Serial.print(':');
-  Serial.println(port);
+  if (client) {
+    if (client.connected())
+    {
+      Serial.println("Client Connected");
+    }
 
+    while (client.connected()) {
+      while (client.available() > 0) {
+        char ch = static_cast<char>(client.read());
+        Serial.print(ch);
+        if ch == "right"
+      {
+        Serial.println("right");
+          digitalWrite( pin_ENA, 0 ); // Разрешаем работу двигателя.
+          delayMicroseconds(5); // Выполняем задержку t1 (см. график STEP/DIR).
+          digitalWrite( pin_DIR, 0 ); // Выбираем направление вращения.
 
+          delayMicroseconds(5);
+          for (int i = 0; i < 800; i++) { // Выполняем 1600 проходов цикла (1 оборот = 800 тактов, 2 оборота = 1600).
+            digitalWrite( pin_PUL, 1 ); // Устанавливаем на выводе PUL состояние логической «1».
+            delayMicroseconds(t); // Выполняем задержку t3 (см. график STEP/DIR).
+            digitalWrite( pin_PUL, 0 ); // Устанавливаем на выводе PUL состояние логического «0».
+            delayMicroseconds(t); // Выполняем задержку t4 (см. график STEP/DIR).
+          }
+        }
+        if ch == "left"
+      {
+        Serial.println("left");
+          digitalWrite( pin_ENA, 0 ); // Разрешаем работу двигателя.
+          delayMicroseconds(5); // Выполняем задержку t1 (см. график STEP/DIR).
+          digitalWrite( pin_DIR, 1 ); // Выбираем направление вращения.
+
+          delayMicroseconds(5);
+          for (int i = 0; i < 800; i++) { // Выполняем 1600 проходов цикла (1 оборот = 800 тактов, 2 оборота = 1600).
+            digitalWrite( pin_PUL, 1 ); // Устанавливаем на выводе PUL состояние логической «1».
+            delayMicroseconds(t); // Выполняем задержку t3 (см. график STEP/DIR).
+            digitalWrite( pin_PUL, 0 ); // Устанавливаем на выводе PUL состояние логического «0».
+            delayMicroseconds(t); // Выполняем задержку t4 (см. график STEP/DIR).
+          }
+        }
+        if ch == "stop"
+      {
+        Serial.println("stop");
+          digitalWrite( pin_ENA, 1 );
+        }
+
+        //INA219
+
+        if ch == "getcurrent_mA"
+      {
+        Serial.println(getcurrent_mA());
+          sendcurrent_mA()
+        }
+      }
+    }
+    client.stop();
+    Serial.println("Client disconnected");
+  }
+}
+
+void sendcurrent_mA() {
   WiFiClient client;
-  if (!client.connect(host, port)) {
-    Serial.println("connection failed");
-    delay(5000);
-    return;
+
+  Serial.printf("\n[Connecting to %s ... ", host);
+  if (client.connect(host, 80))
+  {
+    Serial.println("connected");
+
+    Serial.println("[Sending a request]");
+    client.print(getcurrent_mA())
+    
+    client.stop();
+    Serial.println("\n[Disconnected]");
   }
-
-
-  Serial.println("sending data to server");
-  if (client.connected()) {
-    client.println("hello from ESP8266");
+  else
+  {
+    Serial.println("connection failed!]");
+    client.stop();
   }
-
-  // wait for data to be available
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      delay(6000);
-      return;
-    }
-  }
-
-  // Read all the lines of the reply from server and print them to Serial
-  Serial.println("receiving from remote server");
-  // not testing 'client.connected()' since we do not need to send data here
-  while (client.available()) {
-    char ch = static_cast<char>(client.read());
-    Serial.print(ch);
-    if ch == "right"
-    {
-      Serial.println("right");
-      digitalWrite( pin_ENA, 0 ); // Разрешаем работу двигателя.
-      delayMicroseconds(5); // Выполняем задержку t1 (см. график STEP/DIR).
-      digitalWrite( pin_DIR, 0 ); // Выбираем направление вращения.
-
-      delayMicroseconds(5);
-      for (int i = 0; i < 800; i++) { // Выполняем 1600 проходов цикла (1 оборот = 800 тактов, 2 оборота = 1600).
-        digitalWrite( pin_PUL, 1 ); // Устанавливаем на выводе PUL состояние логической «1».
-        delayMicroseconds(t); // Выполняем задержку t3 (см. график STEP/DIR).
-        digitalWrite( pin_PUL, 0 ); // Устанавливаем на выводе PUL состояние логического «0».
-        delayMicroseconds(t); // Выполняем задержку t4 (см. график STEP/DIR).
-      }
-    }
-    if ch == "left"
-    {
-      Serial.println("left");
-      digitalWrite( pin_ENA, 0 ); // Разрешаем работу двигателя.
-      delayMicroseconds(5); // Выполняем задержку t1 (см. график STEP/DIR).
-      digitalWrite( pin_DIR, 1 ); // Выбираем направление вращения.
-
-      delayMicroseconds(5);
-      for (int i = 0; i < 800; i++) { // Выполняем 1600 проходов цикла (1 оборот = 800 тактов, 2 оборота = 1600).
-        digitalWrite( pin_PUL, 1 ); // Устанавливаем на выводе PUL состояние логической «1».
-        delayMicroseconds(t); // Выполняем задержку t3 (см. график STEP/DIR).
-        digitalWrite( pin_PUL, 0 ); // Устанавливаем на выводе PUL состояние логического «0».
-        delayMicroseconds(t); // Выполняем задержку t4 (см. график STEP/DIR).
-      }
-    }
-    if ch == "stop"
-    {
-      Serial.println("stop");
-      digitalWrite( pin_ENA, 1 );
-    }
-
-    //INA219
-    if ch == "getbusvoltage"
-    {
-      Serial.println(getbusvoltage());
-      client.print(getbusvoltage());
-    }
-
-    if ch == "getpower_mW"
-    {
-      Serial.println(getpower_mW());
-      client.print(getpower_mW());
-    }
-
-    if ch == "getcurrent_mA"
-    {
-      Serial.println(getcurrent_mA());
-      client.print(getcurrent_mA());
-    }
-  }
-
-  // Close the connection
-  Serial.println();
-  Serial.println("closing connection");
-  client.stop();
-
-  if (wait) {
-    delay(3000); // execute once every 5 minutes, don't flood remote service
-  }
-  wait = true;
 }
